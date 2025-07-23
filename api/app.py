272001ef13fb11019d45d82150b5ede12c502ef8
CaioCesarMDS <caiocesarsts@gmail.com>
2025-07-23 18:19:31 -0300

    feat: add file upload

--- api/app.py
+++ api/app.py
@@ -1,27 +1,55 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
-from fastapi.responses import JSONResponse
+from src.main import main
+
 import pandas as pd
+import zipfile
 from io import BytesIO
-from sklearn.ensemble import HistGradientBoostingRegressor
+
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
-@app.post("/predict")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/api/predict")
 async def predict(file: UploadFile = File(...)):
     if not file.filename.endswith(".xlsx"):
         raise HTTPException(status_code=400, detail="O arquivo precisa ser .xlsx")
 
-    contents = await file.read();
+    contents = await file.read()
 
     try:
         df = pd.read_excel(BytesIO(contents))
     except Exception as e:
-      raise HTTPException(status_code=400, detail=f"Erro ao ler o arquivo: {str(e)}")
+        raise HTTPException(status_code=400, detail=f"Erro ao ler o arquivo: {str(e)}")
 
-    model = HistGradientBoostingRegressor(l2_regularization=1.0, learning_rate=0.2, max_depth=30,max_iter=450, min_samples_leaf=5, random_state=42)
+    predictions = main(df)
+    df["Predicted_Price"] = predictions
 
-    predictions = model.predict(df)
+    excel_buffer = BytesIO()
+    df.to_excel(excel_buffer, index=False)
+    excel_buffer.seek(0)
 
-    df['Predicted_Price'] = predictions
+    txt_buffer = BytesIO()
+    txt_buffer.write("\n".join(map(str, predictions)).encode())
+    txt_buffer.seek(0)
 
-    return JSONResponse(content={"predictions": predictions.tolist()})
+    zip_buffer = BytesIO()
+    with zipfile.ZipFile(zip_buffer, "w") as zip_file:
+        zip_file.writestr("predictions.txt", txt_buffer.read())
+        zip_file.writestr("flightprice_com_predicoes.xlsx", excel_buffer.read())
+    zip_buffer.seek(0)
+
+    return StreamingResponse(
+        zip_buffer,
+        media_type="application/zip",
+        headers={"Content-Disposition": f"attachment; filename=resultado.zip"}
+    )
